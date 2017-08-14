464de2d3411b0b3da55ae9bdbdfc1e8885596650
madhead <siarhei.krukau@gmail.com>
2017-08-15 02:46:13 +0300

    UI configuration support for `doktor` step

--- src/main/kotlin/by/dev/madhead/doktor/pipeline/DoktorStep.kt
+++ src/main/kotlin/by/dev/madhead/doktor/pipeline/DoktorStep.kt
@@ -1,8 +1,17 @@
 package by.dev.madhead.doktor.pipeline
 
+import by.dev.madhead.doktor.Messages
+import by.dev.madhead.doktor.config.ConfluenceServers
+import com.cloudbees.plugins.credentials.CredentialsMatchers
+import com.cloudbees.plugins.credentials.common.StandardListBoxModel
+import com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials
 import hudson.Extension
+import hudson.FilePath
 import hudson.model.Run
 import hudson.model.TaskListener
+import hudson.security.ACL
+import hudson.util.ListBoxModel
+import jenkins.model.GlobalConfiguration
 import org.jenkinsci.plugins.workflow.steps.Step
 import org.jenkinsci.plugins.workflow.steps.StepContext
 import org.jenkinsci.plugins.workflow.steps.StepDescriptor
@@ -12,7 +21,9 @@ import org.kohsuke.stapler.DataBoundConstructor
 class DoktorStep
 @DataBoundConstructor
 constructor(
-	val message: String
+	val server: String,
+	val markdownPattern: String,
+	val asciidocPattern: String
 ) : Step() {
 	override fun start(context: StepContext) = Execution(context)
 
@@ -20,15 +31,24 @@ constructor(
 	class DoktorStepDescriptor : StepDescriptor() {
 		override fun getFunctionName() = "doktor"
 
-		override fun getDisplayName() = "Publish documentation to Confluence"
+		override fun getDisplayName() = Messages.doktor_pipeline_DoktorStep_displayName()
 
-		// TODO: Implement this method wisely
-		override fun getRequiredContext() = setOf(Run::class.java)
+		override fun getRequiredContext() = setOf(FilePath::class.java)
+
+		fun doFillServerItems(): ListBoxModel {
+			val result = ListBoxModel()
+
+			GlobalConfiguration.all().get(ConfluenceServers::class.java)?.servers?.forEach {
+				result.add("${it.name} (${it.url})", it.name)
+			}
+
+			return result
+		}
 	}
 
 	inner class Execution(context: StepContext) : SynchronousStepExecution<Unit?>(context) {
 		override fun run(): Unit? {
-			return context.get(TaskListener::class.java)?.logger?.println(message)
+			return context.get(TaskListener::class.java)?.logger?.println("Server: ${server}, MD: ${markdownPattern}, AD: ${asciidocPattern}")
 		}
 	}
 }
