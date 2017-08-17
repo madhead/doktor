0496e1380bda67a8b75df335d60aa5b7a3379f12
madhead <siarhei.krukau@gmail.com>
2017-08-17 15:28:39 +0300

    Improve UI for DoktorStep

--- src/main/kotlin/by/dev/madhead/doktor/pipeline/DoktorStep.kt
+++ src/main/kotlin/by/dev/madhead/doktor/pipeline/DoktorStep.kt
@@ -5,7 +5,6 @@ import by.dev.madhead.doktor.config.ConfluenceServers
 import hudson.Extension
 import hudson.FilePath
 import hudson.model.TaskListener
-import hudson.util.FormValidation
 import hudson.util.ListBoxModel
 import jenkins.model.GlobalConfiguration
 import org.jenkinsci.plugins.workflow.steps.Step
@@ -13,16 +12,15 @@ import org.jenkinsci.plugins.workflow.steps.StepContext
 import org.jenkinsci.plugins.workflow.steps.StepDescriptor
 import org.jenkinsci.plugins.workflow.steps.SynchronousStepExecution
 import org.kohsuke.stapler.DataBoundConstructor
-import org.kohsuke.stapler.QueryParameter
 
 class DoktorStep
 @DataBoundConstructor
 constructor(
 	val server: String,
-	val markdownIncludePattern: String,
-	val markdownExcludePattern: String,
-	val asciidocIncludePattern: String,
-	val asciidocExcludePattern: String
+	var markdownIncludePatterns: List<String>?,
+	var markdownExcludePatterns: List<String>?,
+	var asciidocIncludePatterns: List<String>?,
+	var asciidocExcludePatterns: List<String>?
 ) : Step() {
 	override fun start(context: StepContext) = Execution(context)
 
@@ -47,7 +45,7 @@ constructor(
 
 	inner class Execution(context: StepContext) : SynchronousStepExecution<Unit?>(context) {
 		override fun run(): Unit? {
-			return context.get(TaskListener::class.java)?.logger?.println("Server: ${server}, +MD: ${markdownIncludePattern}, -MD: ${markdownExcludePattern}, +AD: ${asciidocIncludePattern}, -AD: ${asciidocExcludePattern}")
+			return context.get(TaskListener::class.java)?.logger?.println(this@DoktorStep.toString())
 		}
 	}
 }
