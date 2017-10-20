3ec31cbee01470087c73e1be156d79d4d6d9acd9
madhead <siarhei.krukau@gmail.com>
2017-10-21 00:30:14 +0300

    Fix SSH config

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,14 +3,8 @@
 set -x
 
 if [[ ${TRAVIS_BRANCH} == 'master' ]] && [[ ${TRAVIS_PULL_REQUEST} == 'false' ]] &&  [[ $(git cat-file -p ${TRAVIS_COMMIT} | grep -o 'parent' | wc -l) -gt 1 ]]; then
-	SSH_FILE="$HOME/.ssh/id_rsa"
-
-	openssl aes-256-cbc -K ${encrypted_6ae40059dfe6_key} -iv ${encrypted_6ae40059dfe6_iv} -in .travis/github_deploy_key.enc -out ${SSH_FILE} -d
-	chmod 600 ${SSH_FILE}
-	cat <<- EOF >> ${HOME}/.ssh/config
-		Host github.com
-			IdentityFile ${SSH_FILE}
-	EOF
+	openssl aes-256-cbc -K ${encrypted_6ae40059dfe6_key} -iv ${encrypted_6ae40059dfe6_iv} -in .travis/github_deploy_key.enc -out ${HOME}/.ssh/id_rsa -d
+	mv -f .travis/config ${HOME}/.ssh/config
 	git checkout ${TRAVIS_BRANCH}
 
 	./gradlew clean release -Prelease.useAutomaticVersion=true
