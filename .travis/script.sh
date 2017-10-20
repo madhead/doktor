ff2502fddb610a3c925394356e620c8854e47789
madhead <siarhei.krukau@gmail.com>
2017-10-20 23:54:33 +0300

    Damn that bash!

--- .travis/script.sh
+++ .travis/script.sh
@@ -3,7 +3,7 @@
 set -x
 
 if [[ ${TRAVIS_BRANCH} == 'master' ]] && [[ ${TRAVIS_PULL_REQUEST} == 'false' ]] &&  [[ $(git cat-file -p ${TRAVIS_COMMIT} | grep -o 'parent' | wc -l) -gt 1 ]]; then
-	declare -r SSH_FILE=$(mktemp -u $HOME/.ssh/id_rsa)
+	SSH_FILE="$HOME/.ssh/id_rsa"
 
 	openssl aes-256-cbc -K ${encrypted_6ae40059dfe6_key} -iv ${encrypted_6ae40059dfe6_iv} -in .travis/github_deploy_key.enc -out ${SSH_FILE} -d
 	chmod 600 ${SSH_FILE}
