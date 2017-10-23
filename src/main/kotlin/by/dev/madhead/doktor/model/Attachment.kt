13e26cf4723b76e8576e05bad0c6dd3bab5f3079
madhead <siarhei.krukau@gmail.com>
2017-10-23 03:44:14 +0300

    Fix #36: wrap generated content in div with predefined class

--- src/main/kotlin/by/dev/madhead/doktor/model/Attachment.kt
+++ src/main/kotlin/by/dev/madhead/doktor/model/Attachment.kt
@@ -1,8 +1,28 @@
 package by.dev.madhead.doktor.model
 
 import java.io.Serializable
+import java.util.Arrays
 
 data class Attachment(
 	val fileName: String,
 	val bytes: ByteArray
-) : Serializable
+) : Serializable {
+	override fun equals(other: Any?): Boolean {
+		if (this === other) return true
+		if (javaClass != other?.javaClass) return false
+
+		other as Attachment
+
+		if (fileName != other.fileName) return false
+		if (!Arrays.equals(bytes, other.bytes)) return false
+
+		return true
+	}
+
+	override fun hashCode(): Int {
+		var result = fileName.hashCode()
+
+		result = 31 * result + Arrays.hashCode(bytes)
+		return result
+	}
+}
---
+++ src/test/resources/by/dev/madhead/doktor/render/DokRenderer/md/zero_image.md
@@ -0,0 +1,7 @@
+---
+title: zero_image.md
+---
+
+You can't see this image, it is broken:
+
+![Zeros](../zero.png "Zeros")
